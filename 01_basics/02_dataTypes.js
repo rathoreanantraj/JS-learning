0119103e663a7d5daee332829963d2855cdd3be6
Anant Raj <anantraj7050190911@gmail.com>
2024-10-29 12:27:44 +0530

    Data types basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";   //Treats all JS code as newer version
+
+// alert(3 + 3)  //We are using node.js, not browser
+
+// number => 2^53
+// big int
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object => most important
+
+console.log(typeof "Rathore")
+console.log(typeof 123)
+console.log(typeof null)  // object
+console.log(typeof undefined)  // undefined
