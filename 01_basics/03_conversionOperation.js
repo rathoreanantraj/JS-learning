fa1106baa22fc54694b775f0a450208b5b64cef8
Anant Raj <anantraj7050190911@gmail.com>
2024-11-01 10:22:05 +0000

    01_basics/03_conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let num = 12345
+console.log(typeof num);
+console.log(num);
+
+let strNum = String(num)
+console.log(typeof strNum)
+console.log(strNum)
+
+let str = "Rathore"
+console.log(typeof str)
+console.log(str)
+
+let numStr = Number(str)
+console.log(typeof numStr)
+console.log(numStr)  //NaN (Not a Number)
+
+let number = 1
+console.log(typeof number)
+console.log(number)
+
+let bool = Boolean(number)
+console.log(typeof bool)
+console.log(bool)
+
+
+/***************** OPERATION ****************/
+let val = 3
+console.log(-val)
+
+console.log(2**3)  //2^3
+
+let str1 = "Anant Raj"
+let str2 = " Rathore"
+let str3 = str1 + str2
+console.log(str3)
